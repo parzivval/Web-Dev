e1ee5de2ffde5202a6b36b0f8dd820aa02a7b697
parzivval <123867960+parzivval@users.noreply.github.com>
2023-02-22 17:30:47 +0600

    Add files via upload

---
+++ Lab 4/src/app/products.ts
@@ -0,0 +1,108 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  rating: number;
+  description: string;
+  url: string;
+  link: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 14 Pro Max 256Gb фиолетовый',
+    price: 702330,
+    rating: 5.0,
+    description: 'Телефон Apple',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h61/62948780736542/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+  },
+  {
+    id: 2,
+    name: 'Смартфон Samsung Galaxy A33 5G 6 ГБ/128 ГБ черный',
+    price: 129680,
+    rating: 5.0,
+    description: 'Телефон Samsung',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h56/h8c/49894748160030/samsung-galaxy-a33-5g-6-gb-128-gb-cernyj-104398547-1.jpg',
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-a33-5g-6-gb-128-gb-chernyi-104398547/?c=750000000#!/item',
+  },
+  {
+    id: 3,
+    name: 'Смартфон Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+    price: 128350,
+    rating: 5.0,
+    description: 'Телефон Xiaomi',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h45/h27/64487158087710/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg',
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000#!/item',
+  },
+  {
+    id: 4,
+    name: 'Смартфон Apple iPhone 13 128Gb белый',
+    price: 373470,
+    rating: 5.0,
+    description: 'Телефон Apple',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h05/h2c/46392664195102/apple-iphone-13-128gb-belyj-102298420-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-belyi-102298420/?c=750000000#!/item',
+  },
+  {
+    id: 5,
+    name: 'Смартфон Xiaomi Redmi 10C 4 ГБ/128 ГБ серый',
+    price: 71978,
+    rating: 5.0,
+    description: 'Телефон Xiaomi',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hea/h96/49939925139486/xiaomi-redmi-10c-4-gb-128-gb-seryj-104417231-1.jpg',
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-seryi-104417231/?c=750000000#!/item',
+  },
+  {
+    id: 6,
+    name: 'Смартфон Apple iPhone 14 128Gb голубой',
+    price: 71978,
+    rating: 5.0,
+    description: 'Телефон Apple',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8b/h41/63073057177630/apple-iphone-14-128gb-goluboj-106363150-1.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000#!/item',
+  },
+  {
+    id: 7,
+    name: 'Смартфон Samsung Galaxy A03 Core 2 ГБ/32 ГБ черный',
+    price: 46279,
+    rating: 5.0,
+    description: 'Телефон Samsung',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha4/hce/47719147929630/samsung-galaxy-a03-core-2-32gb-cernyj-103153817-1.jpg',
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-a03-core-2-gb-32-gb-chernyi-103153817/?c=750000000#!/item',
+  },
+  {
+    id: 8,
+    name: 'Смартфон Apple iPhone 13 128Gb розовый',
+    price: 46279,
+    rating: 5.0,
+    description: 'Телефон Apple',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha0/h0e/46392660819998/apple-iphone-13-128gb-rozovyj-102298145-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-rozovyi-102298145/?c=750000000#!/item',
+  },
+  {
+    id: 9,
+    name: 'Смартфон Poco X4 Pro 8 ГБ/256 ГБ черный',
+    price: 177000,
+    rating: 5.0,
+    description: 'Телефон Poco',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h91/h47/49566152032286/poco-x4-pro-8-256gb-cernyj-104227418-1.jpg',
+    link: 'https://kaspi.kz/shop/p/poco-x4-pro-8-gb-256-gb-chernyi-104227418/?c=750000000#!/item',
+  },
+  {
+    id: 10,
+    name: 'Смартфон Apple iPhone 12 64Gb черный',
+    price: 322100,
+    rating: 5.0,
+    description: 'Телефон Apple',
+    url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h35/h3d/33279115886622/apple-iphone-12-64gb-cernyj-100656839-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-12-64gb-chernyi-100656839/?c=750000000#!/item',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
